7b22eaa6b90c30c1db1428661eb585c92864ad1e
20141105052 <xiaoods3on1@sina.com>
2015-11-18 20:11:40 +0800

    第一次修改
    不需要判断和输入电源

--- sketch_nov18a/sketch_nov18a.ino
+++ sketch_nov18a/sketch_nov18a.ino
@@ -5,10 +5,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int x;
-  x=digitalRead(10);
-  if(x==HIGH)
-  {
+
     digitalWrite(13,HIGH);
     delay(250);//延时250毫秒
     digitalWrite(13,LOW);
@@ -17,5 +14,5 @@ void loop() {
     delay(50);//延时50毫秒
     digitalWrite(13,LOW);
     delay(50);
-  }
+  
 }
